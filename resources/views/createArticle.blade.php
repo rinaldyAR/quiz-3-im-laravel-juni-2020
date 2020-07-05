3d19a7f93aaab022536bdd701e665fb3d6d3f998
rinaldyAR <rinaldardyans@gmail.com>
2020-07-05 19:57:34 +0700

    update Quiz 3.
    Signed-off-by: rinaldyAR <rinaldardyans@gmail.com>

--- resources/views/createArticle.blade.php
+++ resources/views/createArticle.blade.php
@@ -3,18 +3,18 @@
 @section('content')
 <form action="/article" method="POST">
 @csrf
-<label for="judul">Judul</label>
+<label for="judul">Judul</label><br>
 <input type="text" name="judul" id="judul">
 <br>
-<label for="isi">Konten</label>
+<label for="isi">Konten</label><br>
 <textarea type="text" name="isi" id="isi"></textarea>
 <br>
-<label for="tag">Tag</label>
+<label for="tag">Tag (pisah dengan koma)</label><br>
 <input type="text" name="tag" id="tag">
 <br>
-<label for="writer">Id Penulis</label>
+<label for="writer">Id Penulis</label><br>
 <input type="text" name="writer" id="writer">
-<br>
+<br><br>
 
 <input type="submit" value="Submit">
 </form>
